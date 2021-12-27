6e47285f71a19a515c512c3a9d1b258ba22cfe10
radwanromy <radwanromy@gmail.com>
2021-12-27 11:11:16 +0600

    LS FE AG 012

--- src/app/locker-service-dashboard/locker-service-dashboard.component.ts
+++ src/app/locker-service-dashboard/locker-service-dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormBuilder, FormGroup } from '@angular/forms';
+
 @Component({
   selector: 'app-locker-service-dashboard',
   templateUrl: './locker-service-dashboard.component.html',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LockerServiceDashboardComponent implements OnInit {
 
-  constructor() { }
+  formValue !: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.formValue = this.formBuilder.group({
+      fname : [''],
+      lname : [''],
+      eid : [''],
+      mno : [''],
+      sal : ['']
+    })
   }
 
 }
